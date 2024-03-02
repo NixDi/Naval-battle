9f5449b0ec589c10c31601dce985fdd24cc6a05c
Dima Nafanets <dima.nafanets@gmail.com>
2024-03-03 00:08:10 +0200

    Add some functions

--- Naval battle.py
+++ Naval battle.py
@@ -1,9 +1,22 @@
 #МОРСКОЙ БОЙ
 from random import randint
 
+accept_board = False
+input_board = input('Давай настроим поле.\nВведи число от 5 до 25 (это будет ширина и высота поля):')
+print(input_board)
+while accept_board is False:
+    if int(input_board) >= 5 and int(input_board) <= 25:
+        accept_board = True
+    else:
+        if int(input_board) < 5 or int(input_board) > 25:
+                print("Будь внимательнее!")
+                input_board = input("Неоходимо ввести число ОТ 5 ДО 25:")
+        
 board = []
-for i in range(5):
-    board.append(["O"] * 5)
+
+for i in range(int(input_board)):
+    board.append(["O"] * int(input_board))
+
 
 def print_board(board):
     for row in board:
@@ -22,10 +35,10 @@ ship_col = random_col(board)
 print_board(board)
 #print(ship_row)
 #print(ship_col)
-print("У тебя 5 попыток!")
+print("У тебя " + str(input_board) + " попыток!")
 
-for turn in range(6):
-    print("Попытка " + str(turn) + "/5! Удачи!)")
+for turn in range(int(input_board) + 1):
+    print("Попытка " + str(turn) + "/" + str(input_board) + "! Удачи!)")
     guess_row = int(input("Угадай строку: "))
     guess_col = int(input("Угадай столбик: "))
 
@@ -33,24 +46,24 @@ for turn in range(6):
         print("Поздравляю! Ты потопил мой корабль!)")
         break
     else:
-        if guess_row not in range(5) or guess_col not in range(5):
+        if guess_row not in range(int(input_board)) or guess_col not in range(int(input_board)):
             print("Ты ударил мимо нашего моря!")
         elif board [guess_row] [guess_col] == "X":
             print("В это место ты уже стрелял! Зря только снаряд потратил...")
         else:
-            if turn < 2:
+            if turn < int(input_board) / 3:
                 print("Извини, но ты промазал")
-            elif turn < 4:
+            elif turn < int(input_board) / 2:
                 print("Мимо!")
-            elif turn < 5:
+            elif turn < int(input_board):
                 print("Целься лучше!")
             else:
                 print("Мазила! Игра окончена!")
-            if turn < 5:
+            if turn < int(input_board):
                 print("Попытка", turn + 1)
             board [guess_row] [guess_col] = "X"
-            if turn < 5:
+            if turn < int(input_board):
                 print_board(board)
-        if turn == 5:
+        if turn == int(input_board):
             board[ship_row][ship_col] = "V"
             print_board(board)
